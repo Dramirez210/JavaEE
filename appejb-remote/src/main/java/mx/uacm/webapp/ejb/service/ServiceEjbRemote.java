0653b98a59a0acbd084520974fd7c7237af87807
Daniel Ramirez <danieldosdr@gmail.com>
2024-05-01 21:42:25 -0600

    Comunicacion remota @Remote

---
+++ appejb-remote/src/main/java/mx/uacm/webapp/ejb/service/ServiceEjbRemote.java
@@ -0,0 +1,13 @@
+package mx.uacm.webapp.ejb.service;
+
+import jakarta.ejb.Remote;
+import mx.uacm.webapp.ejb.models.Producto;
+
+import java.util.List;
+
+@Remote
+public interface ServiceEjbRemote {
+    String saludar(String nombre);
+    List<Producto> listar();
+    Producto crear(Producto producto);
+}
